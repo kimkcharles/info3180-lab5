f15a0ebb050e88c8e1d59029dbb317b1e4214ac9
Kimberly Charles <kimberly.charles@mymona.uwi.edu>
2019-02-26 19:19:46 +0000

    Step 6 completed

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "SHuU Coto RkcDvDk"
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://lab5:lab5k@localhost/lab5"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://lab5:lab5info@localhost/lab5"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # added just to suppress a warning
 
 db = SQLAlchemy(app)
