8fea2f2cab73a31fcc6667bcc7c9ec4fab253a4b
Kimberly Charles <kimberly.charles@mymona.uwi.edu>
2019-03-02 06:30:31 +0000

    Step 9 completed

--- app/views.py
+++ app/views.py
@@ -28,6 +28,11 @@ def about():
     """Render the website's about page."""
     return render_template('about.html')
 
+@app.route('/secure-page/')
+@login_required
+def secure_page():
+    """Render the website's secure page only if the user is logged in."""
+    return render_template('secure_page.html')
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -54,13 +59,18 @@ def login():
                 
                 # remember to flash a message to the user
                 flash('Logged in successfully.', 'success')
-                return redirect(url_for("secure-page"))  # they should be redirected to a secure-page route instead
+                return redirect(url_for("secure_page"))  # they should be redirected to a secure-page route instead
                 
             else:
                 flash('Username or Password is incorrect.', 'danger')
                 
     return render_template("login.html", form=form)
 
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out', 'success')
+    return redirect(url_for('home'))
 
 # user_loader callback. This callback is used to reload the user object from
 # the user ID stored in the session
